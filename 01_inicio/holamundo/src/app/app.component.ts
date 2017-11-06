9d08b07f841511032227f83f7a71484654e1d064
caraguado <caraguado@gmail.com>
2017-11-06 17:02:40 +0100

    primer ejercicio

---
+++ 01_inicio/holamundo/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app curso angular 5 badajoz';
+  logo = '../assets/angular-logo.svg';
+}
